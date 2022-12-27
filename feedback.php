f50b40670988a48a683e151bbfb8b0b2a407e789
pushkarguptaaa <pushkardps8@gmail.com>
2022-12-27 12:41:33 +0530

    if and foreach shorthands

--- feedback.php
+++ feedback.php
@@ -1,31 +1,43 @@
 <?php include './inc/header.php'; ?>
-        <h2>Feedback</h2>
 
-        <div class="card my-3">
-          <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-            molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-            repellendus voluptatibus natus deserunt sed doloribus inventore,
-            totam labore maxime perferendis!
+<?php  
+$feedback = [
+  [
+    'id' => '1',
+    'name' => 'Beth Williams',
+    'email' => 'beth@gmail.com',
+    'body' => 'Pushkar Media is ok'
+  ],
+  [
+    'id' => '2',
+    'name' => 'Ram Sharma',
+    'email' => 'ram@gmail.com',
+    'body' => 'Pushkar Media is excellent'
+  ],
+  [
+    'id' => '3',
+    'name' => 'Brad Pitt',
+    'email' => 'brad@gmail.com',
+    'body' => 'Pushkar Media is bad'
+  ]
+]
+?>
+
+        <h2>Past Feedback</h2>
+
+        <?php if(empty($feedback)): ?>
+         <p class="lead mt-3">There is no feedback</p>
+         <?php endif; ?>
+
+        <?php foreach($feedback as $item): ?>
+        <div class="card my-3 w-75" >
+          <div class="card-body text-center">
+            <?php echo $item['body']; ?>
+            <div class="text-secondary mt-2">
+              By <?php echo $item['name']; ?>
+            </div>
           </div>
         </div>
-
-        <div class="card my-3">
-          <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-            molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-            repellendus voluptatibus natus deserunt sed doloribus inventore,
-            totam labore maxime perferendis!
-          </div>
-        </div>
-
-        <div class="card my-3">
-          <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-            molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-            repellendus voluptatibus natus deserunt sed doloribus inventore,
-            totam labore maxime perferendis!
-          </div>
-        </div>
+        <?php endforeach; ?>
       
 <?php include './inc/footer.php'; ?>
